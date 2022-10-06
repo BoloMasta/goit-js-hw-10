cfb2e3724466add1056140b8a7d6c4617a11c617
Bolek <103572991+BoloMasta@users.noreply.github.com>
2022-10-06 15:09:32 +0200

    main

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
+
+const test = () => {
+    console.log('test');
+  };
+
+var debounce = require('lodash.debounce');
+var debounced = debounce(test, DEBOUNCE_DELAY);
+
+document.querySelector('button').addEventListener('click', debounced);
+
+
+
+
+
+
+
+
+
+Notiflix.Notify.success('Sol lucet omnibus');
+Notiflix.Notify.failure('Qui timide rogat docet negare');
+Notiflix.Notify.warning('Memento te hominem esse');
+Notiflix.Notify.info('Cogito ergo sum');
